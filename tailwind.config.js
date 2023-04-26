2daae322bc88299eac290f669a87d0065cb984f0
7ankash <psyferblock.io@gmail.com>
2023-04-26 16:19:33 +0300

    hello world

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+// /** @type {import('tailwindcss').Config} */
+module.exports = {
+  // purge:[],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+    
+      colors:{
+        ruby: "#ff5c5c",
+        "ruby-tint":"#ffdede",
+        "ruby-shade":"#691c32" ,
+        amethyst:"#9e75ff",
+        "amethyst-tint":"#ece3ff",
+        "amethyst-shade":"#362c77",
+        emerald:"#2cc4ae",
+        "emerald-tint":"#d5f3ef",
+        "emerald-shade":"#003947",
+        pearl:"#fff2e5",
+        diamond:"#ffffff",
+        obsidian:"#151828"
+
+      }
+    },
+  },
+  variants:{
+    extend:{
+      bacgroundColor:["active"],
+
+    },
+  },
+  plugins: [],
+}
