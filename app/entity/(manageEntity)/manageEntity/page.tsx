894f2224422ebe48abba468ca783d6c81285230a
7ankash <psyferblock.io@gmail.com>
2023-06-26 09:06:12 +0300

    clone repo and commits update

--- app/entity/(manageEntity)/manageEntity/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function ManageEntityPage() {
-  return <div>manage entity ManageEntityPage</div>;
-}
-
-export default ManageEntityPage;
--- app/helpers/toUpper.tsx
+++
@@ -1,9 +0,0 @@
-export function toUpper(str) {
-  return str
-    .toLowerCase()
-    .split(" ")
-    .map(function (word: string) {
-      return word[0].toUpperCase() + word.slice(1);
-    })
-    .join(" ");
-}
