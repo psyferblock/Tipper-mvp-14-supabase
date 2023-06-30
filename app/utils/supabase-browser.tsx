95923dee58433b20b53beabaf74d67d0a1c83041
7ankash <psyferblock.io@gmail.com>
2023-06-30 10:02:53 +0300

    added supabase keys

--- app/utils/supabase-browser.tsx
+++ app/utils/supabase-browser.tsx
@@ -6,10 +6,10 @@ import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 // );
 
 export const supabase = createClientComponentClient(
-//     {
-//   supabaseUrl:process.env.NEXT_PUBLIC_SUPABASE_URL,
-//   supabaseKey:process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-// }
+    {
+  supabaseUrl:process.env.NEXT_PUBLIC_SUPABASE_URL,
+  supabaseKey:process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+}
 
 );
 
