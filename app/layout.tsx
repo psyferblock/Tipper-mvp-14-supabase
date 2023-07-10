e5cf76bcca9f480aa26f2f5721e53be6099090ae
7ankash <psyferblock.io@gmail.com>
2023-07-10 12:19:40 +0300

    favicon tipper icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,14 @@
 import { cookies } from "next/headers";
-import { createServerClient } from "./utils/supabase-server";
-
 import "./globals.css";
-// import Navbar from "./root-Components/tools-Components/NavBar";
-// import { AuthContextProvider } from "./context/Store";
-import SupabaseListener from "./supabase-listener";
 import SupabaseProvider from "./supabase-provider";
 import UserInfoContextProvider from "@/app/context/userContext/userContextStore";
 import { getMyUserInfoServer } from "./lib/get/getMyUserInfo";
 import { Work_Sans } from "next/font/google";
-import Head from "next/head";
-import { middleware } from "./middleware";
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-import {} from "@supabase/auth-helpers-nextjs";
 
 export const metadata = {
   icons: {
-    icon: "/icon.png",
+    icon: "/favicon.ico",
   },
   title: "Tipper",
   description: "Network your society",
